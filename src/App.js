2f0b813f0e8e624914b1b6d8eda2d511764c8d2f
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-07-08 12:09:20 +0530

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
 function App() {
-  return <div>Hello Realtor</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="forgot-password" element={<ForgotPassword />} />
+          <Route path="/offers" element={<Offers />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
